7942715c17de6e4b7de083fd82fd4a0d0db3e4c5
Jesus Duran <jesusduran141014@gmail.com>
2024-09-19 18:47:42 +0200

    tarea 20/09

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fdcbebbd8f788d85ffc46d5c63c7b378ff3ea3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb847018ec5c4bf4a2549f764a678a55c8a9723c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
